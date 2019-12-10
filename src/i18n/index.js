5a9d9e88eb7991b5288e1f9bf0a0d6dce28c2f46
Леонид Борисович Рыбаков <40237326+rdsoft06@users.noreply.github.com>
2019-12-10 14:19:28 +0200

    Add files via upload

---
+++ src/i18n/index.js
@@ -0,0 +1,11 @@
+// application
+import enMessages from './messages/en';
+import ruMessages from './messages/ru';
+import frMessages from './messages/fr';
+
+
+export default {
+    en: enMessages,
+    ru: ruMessages,
+    fr: frMessages,
+};
